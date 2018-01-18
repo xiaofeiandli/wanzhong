d33a65ac0dee884e4e46ff291ccce11f9316177d
xiaofeiandli <1348723001@qq.com>
2018-01-18 14:28:27 +0800

    update

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -87,10 +87,7 @@
     jQuery(document).ready(function() {
         Metronic.init();
         $.backstretch([
-                "/images/bg/1.jpg",
-                "/images/bg/2.jpg",
-                "/images/bg/3.jpg",
-                "/images/bg/4.jpg"
+                "/images/bg/1.jpg"
             ], {
                 fade: 1000,
                 duration: 8000
